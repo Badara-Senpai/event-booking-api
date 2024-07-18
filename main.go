4af99640e2d45054334ab623c14b1e213d5eb1c5
Badara <badou017@gmail.com>
2024-07-18 16:04:44 +0000

    Events Operations & Refactor Routing

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"eventbooking.com/rest-api/db"
-	"eventbooking.com/rest-api/models"
+	"eventbooking.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,30 +11,7 @@ func main() {
 
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":3000")
-}
-
-func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
-
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
-	context.JSON(http.StatusOK, gin.H{"message": "Event Created!", "event": event})
 }
